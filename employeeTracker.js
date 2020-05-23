97a3738bf191e6a7208eef8bacc794989e0dd503
Arpita Desai <patelarpita29@gmail.com>
2020-05-22 23:14:03 -0700

    databse created

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -14,3 +15,76 @@ connection.connect(err => {
 
     console.log(`Connection created on ${connection.threadId}`);
 });
+
+getStarted = () => {
+    return inquirer.prompt({
+        name: "choice",
+        type: "list",
+        choices: [
+            "View all employees",
+            "View all employees by department",
+            "View all employees by manager",
+            "Add employee",
+            "Remove employee",
+            "Update employee role",
+            "Update employee's manager",
+            "View all roles",
+            "Add role",
+            "Remove role",
+            "exit"
+        ]
+    }).then(ans => {
+        switch(ans.choice) {
+            case "View all employees":
+            viewAllEmp();
+            break;
+
+            case "View all employees by department":
+            viewAllEmpDep();
+            break; 
+             
+            case  "View all employees by manager":
+            viewManager();
+            break;
+              
+            case "Add employee":
+            addEmp();
+            break;
+
+            case  "Remove employee":
+            removeEmp();
+            break;
+
+            case "Update employee role":
+            updateEmp();
+            break;
+
+            case "Update employee's manager":
+            updateManager();
+            break;
+
+            case "View all roles":
+            viewRoles();
+            break;
+
+            case "Add role":
+            addRole();
+            break;
+
+            case "Remove Role":
+			removeRole();
+			break;
+
+			case "Exit":
+			connection.end();
+			break; 
+
+        }
+    });
+};
+
+const viewAllEmp = () => {
+    const query = connection.query(
+        `SELECT `
+    )
+}
--- schema.sql
+++ schema.sql
@@ -4,22 +4,27 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employee(
-    emp_id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    FOREIGN KEY (role_id) REFERENCES roles(role_id),
-    manager_id INTEGER NULL
+
+
+CREATE TABLE department(
+    department_id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    department_name VARCHAR(255)
 );
 
 CREATE TABLE roles(
     role_id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     title VARCHAR(255),
     salary DECIMAL,
+    department_id INTEGER,
     FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
-CREATE TABLE department(
-    department_id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    depatment_name VARCHAR(255)
+CREATE TABLE employee(
+    emp_id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(role_id),
+    FOREIGN KEY (manager_id) REFERENCES employee(emp_id)
 );
